4c209a9c92fb91e54dc8c7df3d896e4bd2043fae
Ascyt <filip.schauer07@gmail.com>
2024-01-28 17:24:06 +0100

    Added indication when copy button pressed

--- src/app/text-tools/output-textarea/output-textarea.component.ts
+++ src/app/text-tools/output-textarea/output-textarea.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input } from '@angular/core';
+import { ThemeSwitcherService } from '../../theme-switcher/theme-switcher.service';
 
 @Component({
   selector: 'app-output-textarea',
@@ -14,9 +15,20 @@ export class OutputTextareaComponent {
   @Input() description: string|undefined = undefined;
   @Input() hiddenText: string|undefined = undefined;
 
+  showCopiedToast: boolean = false;
+
+  constructor(public themeSwitcher: ThemeSwitcherService) {
+
+  }
+
   copyToClipboard() {
     navigator.clipboard.writeText(this.text).then(() => {
       console.log('Copying to clipboard was successful!');
+
+      this.showCopiedToast = true;
+      setTimeout(() => {
+        this.showCopiedToast = false;
+      }, 1000);
     }, function(err) {
       console.error('Could not copy text: ', err);
     });
