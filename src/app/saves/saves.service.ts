61aec83ab215c83508c9ccf1dc810e5879470ad6
Ascyt <account@ascyt.com>
2024-11-08 08:48:51 +0100

    Added save and load to saves

--- src/app/saves/saves.service.ts
+++ src/app/saves/saves.service.ts
@@ -6,32 +6,38 @@ import { Save } from './save';
 })
 export class SavesService {
   public saves: Save[] = [];
-  currentSaveId: number = 0;
+  private _currentSaveId: number = 0;
+  public get currentSaveId() {
+    return this._currentSaveId;
+  }
+  public set currentSaveId(value: number) {
+    this._currentSaveId = value;
+  }
+
   public get currentSave(): Save|undefined {
     return this.saves.find(save => save.id === this.currentSaveId);
   }
 
   constructor() {
-    //this.saves = JSON.parse(localStorage.getItem('saves') || '[]');
-    this.saves = [
-      {
-        id: 0,
-        name: 'Save 1',
-        text: 'This is the first save.',
-        date: new Date()
-      },
-      {
-        id: 1,
-        name: 'Save 2',
-        text: 'This is the second save.',
-        date: new Date()
-      },
-      {
-        id: 2,
-        name: 'Save 3',
-        text: 'This is the third save.',
-        date: new Date()
-      }
+    this.loadSaves();
+    
+    if (this.saves.length === 0) {
+      this.saves = [
+        {
+          id: 0,
+          name: 'Save 1',
+          text: '',
+          date: new Date()
+        }
     ];
+    }
+  }
+
+  loadSaves() {
+    this.saves = JSON.parse(localStorage.getItem('saves') || '[]');
+  }
+
+  saveSaves() {
+    localStorage.setItem('saves', JSON.stringify(this.saves));
   }
 }
